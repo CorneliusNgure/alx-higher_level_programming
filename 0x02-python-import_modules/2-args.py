2f5f6b080b568c151a8cbd9230cf51c93a651d33
CorneliusNgure <kingcornelius07@gmail.com>
2023-12-09 06:14:17 -0500

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_arguments():
+    num_args = len(sys.argv) - 1
+
+    if num_args == 0:
+        print("0 arguments.")
+        return
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
+
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg))
+
+
+if __name__ == "__main__":
+    print_arguments()
