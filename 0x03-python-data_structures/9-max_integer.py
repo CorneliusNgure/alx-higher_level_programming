8f218e44c51c71f575f5942c7f7615650be29fa8
CorneliusNgure <kingcornelius07@gmail.com>
2023-12-10 13:03:26 -0500

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    for x in my_list:
+        if x > my_list[x]:
+            return x
+        else:
+            return my_list[x]
