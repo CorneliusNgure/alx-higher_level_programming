b09a842e798fddd4354191e8a82abde1644f9e9a
CorneliusNgure <kingconelius07@gmail.com>
2024-01-16 17:04:15 -0500

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+"""
+Function that returns addition of 2 ints
+"""
+
+def add_integer(a, b=98):
+    """
+    Returns: 
+        int: Addition of two ints
+
+    Args: 
+        a (int or float): First argument.
+        b (int or float): Second argument.
+
+    Raises:
+        TypeError: If the args are not ints or floats.
+    """
+
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
+    
+
