e8a49aa99b1e23cafbc91d1662b356b8619f020a
CorneliusNgure <kingconelius07@gmail.com>
2024-01-16 01:10:13 -0500

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+module of classs that lists all the methods and attibutes in it
+"""
+
+
+def lookup(obj):
+    """Returns variables, attributes and methods."""
+    return dir(obj)
