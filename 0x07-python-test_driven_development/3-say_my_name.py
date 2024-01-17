8d83c5d6aad4a8bab728bf83ed0f2ef896cbabb5
CorneliusNgure <kingconelius07@gmail.com>
2024-01-17 12:47:18 -0500

    3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Module that defines a function that prints a string with provided names"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print:
+        A string with provided first and last name
+
+    Args:
+        first_name(str): The first name
+        last_name (str): The last name
+
+    Raises:
+        TypeError: if either the first-name or the last_name isn't a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
