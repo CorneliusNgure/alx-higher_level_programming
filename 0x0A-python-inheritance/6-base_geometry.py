2d79860345c1c8e789a7dea976fc6b173af650e9
CorneliusNgure <kingconelius07@gmail.com>
2024-01-22 08:25:56 -0500

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module defines base geometry class"""
+
+
+class BaseGeometry:
+    """Represents base geometry"""
+
+    def area(self):
+        """Not implemented yet"""
+        raise Exception("area() is not implemented")
