ba64b5a616ebecffcde7a487a5e848048e5f2195
CorneliusNgure <kingconelius07@gmail.com>
2024-01-26 00:02:18 -0500

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a function that dumps an object to JSON"""
+"""Defines a JSON-to-string function"""
 import json
 
 
 def from_json_string(my_str):
-    """Returns a JSON str from a .py data struct object
+    """Returns a .py data struct object from JSON
 
     Args:
         my_str (str): The string object
     Return:
         JSON string
     """
-    return json.dumps(my_str)
+    return json.loads(my_str)
