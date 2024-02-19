f4006c05f9d92946b46f58b6f863d8b7ac784a8d
CorneliusNgure <kingconelius07@gmail.com>
2024-02-19 09:01:03 -0500

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Retrieves the ID and name of cities along with the corrsponding state names
-SELECT cities.id, cities.name AS city_name, states.name AS state_name
+SELECT cities.id AS id, cities.name AS name, states.name AS state_name
 FROM cities
 -- Joins the cities table with the states table on the state_id column
 JOIN states ON cities.state_id = states.id
