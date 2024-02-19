3ce234b4fab909e38f029455b2e1caa43e4dd3db
CorneliusNgure <kingconelius07@gmail.com>
2024-02-19 08:55:35 -0500

    9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Retrieves the ID and name of cities along with the corrsponding state names
+SELECT cities.id, cities.name AS city_name, states.name AS state_name
+FROM cities
+-- Joins the cities table with the states table on the state_id column
+JOIN states ON cities.state_id = states.id
+-- Sorts the results by city ID in ascending order.
+ORDER BY cities.id ASC;
