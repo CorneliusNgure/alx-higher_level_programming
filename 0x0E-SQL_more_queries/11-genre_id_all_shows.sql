a05717570ee3c05906d99383bd456a1e54c98dd4
CorneliusNgure <kingconelius07@gmail.com>
2024-02-20 02:03:01 -0500

    11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Lists all shows in the hbtn_0d_tvshows database that have at least one genere linked
+-- Sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
