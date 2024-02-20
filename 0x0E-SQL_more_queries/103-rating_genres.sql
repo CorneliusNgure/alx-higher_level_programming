b409901917f12aa570a8d19fc6f6c4d91253c7e9
CorneliusNgure <kingconelius07@gmail.com>
2024-02-20 05:17:06 -0500

    103-rating_genres.sql

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- List all genres in htbn_0d_tvshows_rate by their rating
+
+SELECT tg.name AS genre, SUM(tsr.rate) AS rating_sum
+FROM tv_genres tg
+JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
+JOIN tv_show_ratings tsr ON tsg.show_id = tsr.show_id
+GROUP BY tg.name
+ORDER BY rating_sum DESC;
