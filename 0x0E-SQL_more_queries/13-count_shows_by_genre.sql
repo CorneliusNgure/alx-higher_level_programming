270f34ac43e7d793308c8c7699f69e9ac92f7e45
CorneliusNgure <kingconelius07@gmail.com>
2024-02-20 02:48:29 -0500

    13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Lists all genres and no. of shows linked to each.
+
+SELECT tg.name AS genre,
+       COUNT(tsg.show_id) AS number_of_shows
+FROM tv_genres tg
+JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
+GROUP BY tg.id, tg.name
+ORDER BY number_of_shows DESC;
