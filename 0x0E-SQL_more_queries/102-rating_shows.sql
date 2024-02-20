6ecc1f8c28950acb8d672d44331c1a7250cb9453
CorneliusNgure <kingconelius07@gmail.com>
2024-02-20 03:49:22 -0500

    102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- Lists all shows by their rating
 
-SELECT ts.title, SUM(tsr.rate) AS rating_sum
+SELECT ts.title, SUM(tsr.rate) AS rating
 FROM tv_shows ts
 JOIN tv_show_ratings tsr ON ts.id = tsr.show_id
 GROUP BY ts.title
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
