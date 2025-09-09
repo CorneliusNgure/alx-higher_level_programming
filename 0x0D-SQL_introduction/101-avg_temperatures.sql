6e6d30a197518f9a759d63a4cc8e2df0260fad43
CorneliusNgure <122824441+CorneliusNgure@users.noreply.github.com>
2025-09-09 16:53:06 +0300

    Update 101-avg_temperatures.sql
    Displays the average temperature by city ordered by temperature (descending).

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,16 +1,7 @@
 -- Displays the avg temperature by city ordered by
 -- temperature(descending)
 
-SELECT city,
-    AVG(temperature_fahrenheit) AS average_temperature_fahrenheit
-FROM (
-    SELECT city,
-        CASE
-            WHEN (MIN(value) >= -459.67 AND MAX(value) <= 212) THEN AVG(value)  -- Fahrenheit, directly take the average
-            ELSE AVG((value * 9 / 5) + 32)  -- Convert Celsius to Fahrenheit and then take the average
-        END AS temperature_fahrenheit
-    FROM temperatures
-    GROUP BY city
-) AS city_temperatures
+SELECT city, AVG(value) AS avg_temp  
+FROM temperatures
 GROUP BY city
-ORDER BY average_temperature_fahrenheit DESC;
+ORDER BY avg_temp DESC;
