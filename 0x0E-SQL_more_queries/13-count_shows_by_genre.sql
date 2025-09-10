78d4b3ee9c92e6f1b3a4879549a13a684c964a0e
CorneliusNgure <122824441+CorneliusNgure@users.noreply.github.com>
2025-09-10 03:08:21 +0300

    Lists all genres and no. of shows linked to each

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,12 @@
 -- Lists all genres and no. of shows linked to each.
 
-SELECT tg.name AS genre,
-       COUNT(tsg.show_id) AS number_of_shows
-FROM tv_genres tg
-JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
-GROUP BY tg.id, tg.name
+SELECT 
+    tv_genres.name AS genre, 
+    COUNT(*) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres 
+    ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows 
+    ON tv_shows.id = tv_show_genres.show_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
